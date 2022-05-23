69fc7f26fc3e2f7f6c9af9a9864d14224cd6125d
Izabella Golovina <ia.golovina00@gmail.com>
2022-05-23 13:08:08 +0300

    Fix computerPlay(). It uses all three options now Add div to display console log Add functionality on click

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const variant = ['rock', 'paper', 'scissors'];
 let playerScore = 0;
 let computerScore = 0;
 
+const consoleDiv = document.querySelector('.console');
+
 // function game() {
 //     for (let i = 0; i < 5; i++) {
 //         const playerSelection = prompt('Choose your weapon');
@@ -11,44 +13,51 @@ let computerScore = 0;
 // }
 
 function computerPlay() {
-    return variant[Math.floor(Math.random() * 2)];
+    return variant[Math.floor(Math.random() * 3)];
 }
 
-function gameRound(playerSelection, computerSelection){
-    if (playerSelection == computerSelection) console.log('Tie');
+function gameRound(playerSelection){
+    console.log(playerSelection);
+    const computerSelection = computerPlay();
+    console.log(computerSelection);
+    if (playerSelection == computerSelection) consoleDiv.textContent = "It's a tie!";
     else switch(playerSelection) {
         case 'rock':
             if (computerSelection == 'scissors') {
                 playerScore += 1;
-                return 'You Win! Rock beats Scissors';
+                consoleDiv.textContent = 'You Win! Rock beats Scissors';
+                break;
             }
             else {
                 computerScore += 1;
-                return 'You Lose! Paper beats Rock';
+                consoleDiv.textContent = 'You Lose! Paper beats Rock';
+                break;
             }
         case 'paper':
             if (computerSelection == 'rock') {
                 playerScore += 1;
-                return 'You Win! Paper beats Rock';
+                consoleDiv.textContent = 'You Win! Paper beats Rock';
+                break;
             }
             else {
                 computerScore += 1;
-                return 'You Lose! Scissors beats Paper';
+                consoleDiv.textContent = 'You Lose! Scissors beats Paper';
+                break;
             }
         case 'scissors':
             if (computerSelection == 'paper') {
                 playerScore += 1;
-                return 'You Win! Scissors beats Paper';
+                consoleDiv.textContent = 'You Win! Scissors beats Paper';
+                break;
             }
             else {
                 computerScore += 1;
-                return 'You Lose! Rock beats Scissors';
+                consoleDiv.textContent = 'You Lose! Rock beats Scissors';
+                break;
             }
     }
 }
 
-game();
-console.log(playerScore, computerScore);
 if (playerScore == computerScore) console.log("It's a tie!");
 else if (playerScore > computerScore) console.log(`You're a winner! You won ${playerScore} times`);
 else console.log(`Computer is a winner! It won ${computerScore} times`); 
